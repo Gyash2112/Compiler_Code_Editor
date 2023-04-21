94a05e586867a6989ace336450b51cd804f7d831
unknown <gyash.21decgmail.com>
2023-04-21 15:54:09 +0530

    removed api call for running code

--- index.js
+++ index.js
@@ -7,40 +7,40 @@ const path = require('path');
 
 app.use(express.static('assets'));
 app.use(express.urlencoded({extended:false}));
-function getOutput(postdata){
-  return new Promise(function(resolve, reject){
-    var data = qs.stringify({
-      'code': postdata.code,
-      'language': postdata.language,
-      'input': postdata.input   
-  });
+// function getOutput(postdata){
+//   return new Promise(function(resolve, reject){
+//     var data = qs.stringify({
+//       'code': postdata.code,
+//       'language': postdata.language,
+//       'input': postdata.input   
+//   });
 
   
   
-  var config = {
-      method: 'post',
-      url: 'https://api.codex.jaagrav.in',
-      headers: {
-          'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      data : data
-  };
-  var output="";
-  axios(config)
-    .then( function (response) {
-      JSON.stringify(response.data);
-      console.log(response.data);
-      resolve(response.data);
-    })
-    .catch(function (error) {
-      console.log(error);
-      reject(error);
-    });
-    console.log(output);
+//   var config = {
+//       method: 'post',
+//       url: 'https://api.codex.jaagrav.in',
+//       headers: {
+//           'Content-Type': 'application/x-www-form-urlencoded'
+//       },
+//       data : data
+//   };
+//   var output="";
+//   axios(config)
+//     .then( function (response) {
+//       JSON.stringify(response.data);
+//       console.log(response.data);
+//       resolve(response.data);
+//     })
+//     .catch(function (error) {
+//       console.log(error);
+//       reject(error);
+//     });
+//     console.log(output);
     
 
-  })
-}
+//   })
+// }
 app.get('/',function(req,res){
   return res.sendFile(path.join(__dirname,'/index.html'));
 
@@ -48,8 +48,8 @@ app.get('/',function(req,res){
 
 app.post('/runCode',  async function(req,res){
   console.log(req.body);
-  var output = await getOutput(req.body);
-  if(output.error) return res.send(output.error);
+  // var output = await getOutput(req.body);
+  // if(output.error) return res.send(output.error);
   return res.send(output.output);
 })
 
