7e6f8c2a80c00e7fbd24167be1c6e2425b393ab6
unknown <gyash.21decgmail.com>
2023-04-28 16:44:08 +0530

    aaa

--- index.js
+++ index.js
@@ -11,40 +11,6 @@ compiler.init({stats : true});
 
 app.use(express.static('assets'));
 app.use(express.urlencoded({extended:false}));
-// function getOutput(postdata){
-//   return new Promise(function(resolve, reject){
-//     var data = qs.stringify({
-//       'code': postdata.code,
-//       'language': postdata.language,
-//       'input': postdata.input   
-//   });
-
-  
-  
-//   var config = {
-//       method: 'post',
-//       url: 'https://api.codex.jaagrav.in',
-//       headers: {
-//           'Content-Type': 'application/x-www-form-urlencoded'
-//       },
-//       data : data
-//   };
-//   var output="";
-//   axios(config)
-//     .then( function (response) {
-//       JSON.stringify(response.data);
-//       console.log(response.data);
-//       resolve(response.data);
-//     })
-//     .catch(function (error) {
-//       console.log(error);
-//       reject(error);
-//     });
-//     console.log(output);
-    
-
-//   })
-// }
 app.get('/',function(req,res){
   return res.sendFile(path.join(__dirname,'/index.html'));
 
